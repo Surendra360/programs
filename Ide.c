e4b0fe2fbc5ff92dda7d022997a4be6fa4569bd7
Surendra360 <88983549+Surendra360@users.noreply.github.com>
2022-04-07 23:36:06 +0530

    BUDGET_
    input:
    3
    1000 10
    250 50
    1500 50
    output: 
    YES
    NO
    YES

---
+++ Ide.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+ {
+	int t,x,y;
+	scanf("%d",&t);
+	
+	for(int i=1;i<=t;i++)
+	{
+	  
+	scanf("%d %d",&x,&y);
+	if((y*30)<=x)
+	printf("YES\n");
+	else
+	printf("NO\n");
+	}
+	return 0;
+}
